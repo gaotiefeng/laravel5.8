57892c1e84caeecf843c71ff8ec8c92527fc2de6
tiefeng <1096392101@qq.com>
2019-07-07 21:33:50 +0800

    phpmailer

---
+++ app/Http/Controllers/Web/MailerController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+
+class MailerController extends Controller
+{
+
+    public function send() {
+
+        echo 1;die;
+    }
+}
